4488bf192186883bfe3201ec18e735b0eeed59a6
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-12-13 19:31:36 +0530

    Day1 final

--- day1.cpp
+++ day1.cpp
@@ -1,7 +1,22 @@
- #include <iostream>
-
+#include<iostream>
 using namespace std;
 
+void setOrNot(int n, int k){
+    if((n>>k) & 1)
+        cout<<"Set";
+    else
+        cout<<"Not set";
+}
+
 int main(){
-    cout<<"Welcome"<<endl;
+    int n=5, k=1;
+
+    // cout<<"Enter the number you want to check for set:"<<endl;
+    // cin>>n;
+
+    // cout<<"Enter the bit number you want to check:"<<endl;
+    // cin>>k;
+
+    setOrNot(n, k);
+    return 0;
 }
