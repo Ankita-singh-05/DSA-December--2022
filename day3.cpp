b77d8bbcd43e0155c55be49e9653d995d129c280
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-12-14 23:23:22 +0530

    Day3 dsa dec

---
+++ day3.cpp
@@ -0,0 +1,18 @@
+// clear the ith bit of a number.
+
+#include <iostream>
+using namespace std;
+
+int clearTheithBit(int n, int k){
+    return 0;
+
+
+
+}
+
+int main(){
+
+    int n, k;
+    return 0;
+    
+}
